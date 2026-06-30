876a32f3751fefa5e866d82e30b0a35ac74cc3c7
karim salah <karimabdelghany753@gmail.com>
2026-06-30 06:27:37 +0300

    Fix errorcodes file and add dockerfile in shared kernel project

--- SharedKernel/ErrorCodes.cs
+++ SharedKernel/ErrorCodes.cs
@@ -2,65 +2,69 @@
 
 namespace SharedKernel;
 
-//  (Auth & Identity)
-public enum AuthError
+public static class ErrorsCodes
 {
-    EmailExists,
-    WeakPassword,
-    InvalidCredentials,
-    AccountLocked,
-    InvalidOtp,
-    OtpExpired,
-    PasswordMismatch,
-    ResetTokenInvalid,
-    TokenInvalid,
-    TokenExpired
+    //  (Auth & Identity)
+    public enum AuthError
+    {
+        EmailExists,
+        WeakPassword,
+        InvalidCredentials,
+        AccountLocked,
+        InvalidOtp,
+        OtpExpired,
+        PasswordMismatch,
+        ResetTokenInvalid,
+        TokenInvalid,
+        TokenExpired
+    }
+
+    public enum ValidationError
+    {
+        RequiredField,
+        InvalidAge,
+        InvalidWeight,
+        InvalidHeight,
+        InvalidGender,
+        InvalidGoal,
+        InvalidActivity,
+        InvalidFileType,
+        FileTooLarge,
+        InvalidDate
+    }
+    //(Fitness/Calculation Engine - FCE)
+    public enum FceError
+    {
+        StatsNotFound,
+        InvalidCalculation,
+        MetricsNotCalculated,
+        NoMatchingPlan
+    }
+
+    public enum ResourceError
+    {
+        NotFound,
+        WorkoutNotFound,
+        PlanNotFound,
+        ExerciseNotFound,
+        SessionNotFound,
+        MealNotFound,
+        UserNotFound
+    }
+
+    // (Permissions & Rate Limits)
+    public enum RateError
+    {
+        OtpResendTooSoon,
+        PremiumRequired,
+        RateLimitExceeded
+    }
+
+    public enum ServerError
+    {
+        FileUploadFailed,
+        ServiceUnavailable,
+        DatabaseError
+    }
 }
 
-public enum ValidationError
-{
-    RequiredField,
-    InvalidAge,
-    InvalidWeight,
-    InvalidHeight,
-    InvalidGender,
-    InvalidGoal,
-    InvalidActivity,
-    InvalidFileType,
-    FileTooLarge,
-    InvalidDate
-}
- //(Fitness/Calculation Engine - FCE)
-public enum FceError
-{
-    StatsNotFound,
-    InvalidCalculation,
-    MetricsNotCalculated,
-    NoMatchingPlan
-}
-
-public enum ResourceError
-{
-    NotFound,
-    WorkoutNotFound,
-    PlanNotFound,
-    ExerciseNotFound,
-    SessionNotFound,
-    MealNotFound,
-    UserNotFound
-}
-
-// (Permissions & Rate Limits)
-public enum RateError
-{
-    OtpResendTooSoon,
-    PremiumRequired,
-    RateLimitExceeded
-}
-
-public enum ServerError
-{
-    FileUploadFailed,
-    ServiceUnavailable,
-    DatabaseError
-}
